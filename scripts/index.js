c20d5e16abf793c263da45918d7981c1c4a93707
bcharles41078 <62114382+bcharles41078@users.noreply.github.com>
2020-04-27 14:03:04 -0500

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,16 @@
 import shoppingList from './shopping-list.js';
+import item from './items.js';
+
+const itemNames = [ '', 'apples', 'pears' ];
+itemNames.forEach(name => {
+  try {
+    item.validateName(name);
+    // create a new item if name is valid
+    store.items.push(item.create(name));
+  } catch(error) {
+    console.log(`Cannot add item: ${error.message}`);
+  }
+});
 
 const main = function () {
   shoppingList.bindEventListeners();
