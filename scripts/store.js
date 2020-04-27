a2bdba68ce72f6d7a9e3946bb85bd68f2c87d66b
bcharles <bcharles41078@gmail.com>
2020-04-27 17:28:16 -0500

    ok

--- scripts/store.js
+++ scripts/store.js
@@ -29,7 +29,6 @@ export default {
 
   function findAndToggleChecked(id){
     let item = findById(id);
-    console.log(item.checked)
     item.checked = !item.checked;
   }
 
@@ -44,12 +43,12 @@ export default {
     }
   }
   
-  function findAndDelete(id){
+function findAndDelete (id) {
     let item = findById(id);
-    this.items = this.items.filter(element=> item.id!== element.id);
+    this.items = this.items.filter(element => item.id !== element.id);
+}
 
-  }
 
-  function toggleCheckedFilter (){
-    this.hideCheckedItems = !this.hideCheckedItems;
-  }
+function toggleCheckedFilter () {
+  this.hideCheckedItems = !this.hideCheckedItems;
+}
